bb7f52e84e437d799dbe855832eac0822b027f09
SammyMwaura <mwaurasammy500@gmail.com>
2024-10-14 15:05:18 +0300

    gunicorn

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ api.add_resource(Checksession, '/check_session')
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        app.run(debug=True)
+        # app.run(debug=True)
